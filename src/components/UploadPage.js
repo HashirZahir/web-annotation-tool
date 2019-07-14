78a08ca4672dfed94cb7389baccdce4cdffac889
Hashir Zahir <hashirsahad@gmail.com>
2019-07-14 17:31:22 +0800

    Add image uploads with image collection name

--- src/components/UploadPage.js
+++ src/components/UploadPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import firebase from "firebase";
-import FileUploader from "react-firebase-file-uploader";
+import CustomUploadButton from 'react-firebase-file-uploader/lib/CustomUploadButton';
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
@@ -17,6 +17,7 @@ class UploadPage extends React.Component {
 
     this.state = {
       uid: props.uid,
+      image_collection_name: "",
       filenames: [],
       // downloadURLs: [],
       isUploading: false,
@@ -58,32 +59,52 @@ class UploadPage extends React.Component {
     }));
   };
 
+  handleChangeCollectionName = event => {
+    this.setState({ image_collection_name: event.target.value });
+  };
+
   render() {
     return (
       <div className="content-container">
-        <p>Current uid (for debugging): {this.state.uid}</p>
-        <FileUploader
-          accept="image/*"
-          name="image-uploader-multiple"
-          randomizeFilename={false}
-          storageRef={firebase.storage().ref(`users/${this.state.uid}/images`)}
-          onUploadStart={this.handleUploadStart}
-          onUploadError={this.handleUploadError}
-          onUploadSuccess={this.handleUploadSuccess}
-          onProgress={this.handleProgress}
-          multiple
+        <label>Image Collection Name:</label>
+        <input
+          type="text"
+          value={this.state.image_collection_name}
+          name="image_collection_name"
+          onChange={this.handleChangeCollectionName}
         />
+        {this.state.image_collection_name !== "" && (
+          <div name="upload-images">
+            <CustomUploadButton
+              accept="image/*"
+              name="image-uploader-multiple"
+              randomizeFilename={false}
+              storageRef={firebase.storage().ref(`users/${this.state.image_collection_name}/images`)}
+              onUploadStart={this.handleUploadStart}
+              onUploadError={this.handleUploadError}
+              onUploadSuccess={this.handleUploadSuccess}
+              onProgress={this.handleProgress}
+              multiple
+              className="button"
+              >
+              Add Images
+            </CustomUploadButton>
 
-        <p>Progress: {this.state.uploadProgress}</p>
+            <p>Progress: {this.state.uploadProgress}</p>
 
-        <p>Filenames: {this.state.filenames.join(", ")}</p>
+            <p>Filenames: {this.state.filenames.join(", ")}</p>
 
-        {/* <div>
-          {this.state.downloadURLs.map((downloadURL, i) => {
-            return <img key={i} src={downloadURL} />;
-          })}
-        </div> */}
+            {/* <div>
+              {this.state.downloadURLs.map((downloadURL, i) => {
+                return <img key={i} src={downloadURL} />;
+              })}
+            </div> */}
 
+
+          </div>
+        )}
+        
+        <br></br><br></br><br></br>
         <Link to="/">
           <button type="button" className="button">
             Back to Dashboard
